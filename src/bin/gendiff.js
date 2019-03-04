f63247a5594a60cba2e9602913e881da12a7282b
Zipofar <admin@zipofar.ru>
2019-03-04 16:59:12 +0300

    Add gendiff binary

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('Hello')
+import program from 'commander';
+
+program
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and show a difference.')
+  .version('0.0.1', '-V, --version')
+  .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig, options) => {
+    console.log(firstConfig, secondConfig, options.format);
+  })
+  .parse(process.argv);
