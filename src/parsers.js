a2bcfa95f5e55375a0a88fb3d6e35c1453ea65dc
Zipofar <admin@zipofar.ru>
2019-03-06 15:50:21 +0300

    Step 2: refactor 3

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,19 @@
+import _ from 'lodash';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
 const parsers = {
-  json: data => (JSON.parse(data)),
-  yml: data => (yaml.safeLoad(data)),
-  ini: data => (ini.parse(data)),
+  json: data => JSON.parse(data),
+  yml: data => yaml.safeLoad(data),
+  ini: data => ini.parse(data),
 };
 
-export default parsers;
+const parse = (data = '', params = { ext: 'json' }) => {
+  const { ext } = params;
+  if (!_.has(parsers, ext)) {
+    throw new Error(`No parsers for ${ext} extension`);
+  }
+  return parsers[ext](data);
+};
+
+export default parse;
